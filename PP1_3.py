cc682ebb0d1b4c6a0fa3b800f8488ee14faa274c
Eric-Fang-ICD201-01 <eric.fang@student.tdsb.on.ca>
2024-09-18 14:08:51 +0000

    commit

--- PP1_3.py
+++ PP1_3.py
@@ -1,18 +1,16 @@
 
 
 def q1():
-  '''
   print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n")
 
   print("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n")
 
   print("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
-  '''
-  print("* * * * *\n * * * * *\n* * * * *\n * * * * *\n* * * * *\n\n* * * *\n*     *\n*     *\n*     *\n* * * *\n\n* * * * *\n *     *\n  *   *\n   * *\n    *", end = "")
+
 
 def q2():
   print("Hello ", end = "")
-  print("World", end = "")
+  print("World")
   
 
 def q3():
